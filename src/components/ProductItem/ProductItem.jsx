0aaed10eab498f20593f49eff82712d48fb8d288
Ivan Vrublevsky <wrublik.93@mail.ru>
2024-02-29 20:26:04 +0300

    test

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./ProductItem.css"
+import Button from "../Button/Button";
+import BudImg from "../../assets/Bud.png"
+
+const ProductItem = ({ product, className }) => {
+    return (
+        <div className={"product " + className}>
+            <div className={"img"}></div>
+            <div className={"title"}>{product.title}</div>
+            <div className={"description"}>{product.description}</div>
+            <div className={"price"}>
+                <span>Price: <b>{product.price}</b></span>
+            </div>
+            <Button className={"add-btn"}>Подробности</Button>
+        </div>
+    );
+};
+
+export default ProductItem;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import "./ProductList.css";
+import ProductItem from "../ProductItem/ProductItem";
+
+const products = [
+    {
+        id: 1,
+        title: "BYD Han Limited Edition 4WD",
+        price: 500,
+        description: "2023, Электро"
+    },
+    {
+        id: 1,
+        title: "Lamborghini Urus",
+        price: 100,
+        description: "Бензин, 4000 см3"
+    }
+]
 
 const ProductList = () => {
     return (
-        <div>
-            Product list
+        <div className={"list"}>
+            {products.map(item => (
+                <ProductItem key={item.id} product={item} className={"item"} />
+            ))}
         </div>
     );
 };
